8177af742da8619fc68be1ecf7de69a45e368122
zhoushun <69625362+zhoushun98@users.noreply.github.com>
2024-04-17 06:02:49 +0800

    Update docker-entrypoint.sh

--- lsky-pro/docker-entrypoint.sh
+++ lsky-pro/docker-entrypoint.sh
@@ -5,6 +5,7 @@ if [[ ! -e /var/www/html/public/index.php ]]; then
     cp -a /usr/src/lsky-pro/* /var/www/html
     cp -a /usr/src/lsky-pro/.env.example /var/www/html
     chown www-data:www-data -R /var/www/html
+    chmod 755 -R /var/www/html
 fi
 
 exec "$@"
