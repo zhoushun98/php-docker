f25ae649652b8cf57766c92c5ac647d8f2ee8735
zhoushun <69625362+zhoushun98@users.noreply.github.com>
2024-04-17 03:42:11 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ `find /usr/src/typecho | wc -l` != 1 ]]; then
+if [[ ! -e /var/www/html/index.php ]]; then
     mv /usr/src/typecho/* /var/www/html
     chown www-data:www-data -R /var/www/html
 fi
